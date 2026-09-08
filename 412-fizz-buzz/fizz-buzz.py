ea1a9c1a6611becc93de00fcf665be1c774c96ed
Abhay Rawat <164727110+Mr-Spectr@users.noreply.github.com>
2026-09-08 22:47:04 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (10.82%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,15 +1,13 @@
-class Solution:
-    def fizzBuzz(self, n: int) -> List[str]:
-        answer = []
+class Solution(object):
+    def fizzBuzz(self, n):
+        result = []
         for i in range(1, n + 1):
-            if i % 3 == 0 and i % 5 == 0:
-                answer.append("FizzBuzz")
-            elif i % 3 == 0:
-                answer.append("Fizz")
-            elif i % 5 == 0:
-                answer.append("Buzz")
-            else:
-                answer.append(str(i))
-        return answer
-
-        
+            s = ""
+            if i % 3 == 0:
+                s += "Fizz"
+            if i % 5 == 0:
+                s += "Buzz"
+            if not s:
+                s = str(i)
+            result.append(s)
+        return result
